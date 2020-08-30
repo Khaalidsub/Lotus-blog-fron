4c3491a36d91fb51d76a65c5c35df7554c810b6e
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-08-30 21:57:08 +0800

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { NavBar } from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import Home from "./Components/Routes/Home";
-import { DEFAULT_THEME } from "./themes";
-import { applyTheme } from "./utils/theme";
-import { setlocalStorage, getlocalStorage } from "./utils/localStorage";
+
 import Profile from "./Components/Routes/Profile";
 import { Posts } from "./Components/Routes/Posts";
 import { AddPost } from "./Components/Routes/AddPost";
@@ -12,47 +10,17 @@ import { ViewPost } from "./Components/Routes/ViewPost";
 import SignUp from "./Components/Routes/SignUp";
 import SignForm from "./Components/Routes/SignForm";
 import EditProfile from "./Components/Routes/EditProfile";
+import Header from "./Components/Header";
 export interface AppProps {}
 
 export interface AppState {}
 
 class App extends React.Component<AppProps, AppState> {
-  state = {
-    theme: DEFAULT_THEME,
-  };
-  async componentDidMount() {
-    const value = await getlocalStorage("theme");
-    if (value) this.setState({ theme: value });
-    applyTheme(this.state.theme);
-  }
-  async setTheme(theme: string) {
-    this.setState({ theme: theme });
-    applyTheme(theme);
-    await setlocalStorage("theme", theme);
-  }
-  // setDefaultTheme
   render() {
     return (
       <div>
-        <div className="p-5">
-          <div className="mx-auto m-6">
-            {this.state.theme === "default" ? (
-              <button
-                className="text-secondary border border-primary p-3"
-                onClick={() => this.setTheme("dark")}
-              >
-                Apply Dark Theme
-              </button>
-            ) : (
-              <button
-                className="text-secondary border border-primary p-3"
-                onClick={() => this.setTheme("default")}
-              >
-                Apply Light Theme
-              </button>
-            )}
-          </div>
-        </div>
+        <Header />
+
         <div className="sticky top-0 z-50 bg-secondary-background">
           <NavBar />
         </div>
@@ -66,7 +34,7 @@ class App extends React.Component<AppProps, AppState> {
           </div>
         </div>
         <div className="md:block hidden fixed bottom-0 z-20 right-0 p-5">
-          <div className="border-4 border-primary  bg-secondary-background p-4 text-secondary rounded-full text-xl  text-center">
+          <div className="border-2 border-primary  bg-secondary-background p-4 text-secondary rounded-full text-xl  text-center">
             Add a new Post
           </div>
         </div>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,64 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { DEFAULT_THEME } from "../themes";
+import { applyTheme } from "../utils/theme";
 
+import { getlocalStorage, setlocalStorage } from "../utils/localStorage";
+import "../styles/header.css";
 export interface HeaderProps {}
 
 export interface HeaderState {}
 
 class Header extends React.Component<HeaderProps, HeaderState> {
+  state = {
+    theme: DEFAULT_THEME,
+    toggle: true,
+  };
+  async componentDidMount() {
+    const value = await getlocalStorage("theme");
+    if (value)
+      this.setState({
+        theme: value,
+        toggle: value === "dark" ? false : true,
+      });
+
+    applyTheme(this.state.theme);
+  }
+  async setTheme(toggling: boolean) {
+    this.setState({
+      theme: toggling ? "default" : "dark",
+      toggle: toggling,
+    });
+    console.log(this.state.theme, this.state.toggle, toggling);
+
+    applyTheme(this.state.theme);
+    await setlocalStorage("theme", this.state.theme);
+  }
   render() {
-    return <div></div>;
+    return (
+      <div className="p-5">
+        <div className="m-6">
+          <div
+            className="switch h-6 w-12 rounded-full bg-tertiary"
+            data-ison={this.state.toggle}
+            onClick={() => this.setTheme(!this.state.toggle)}
+          >
+            <motion.div
+              className="handle h-6 my-auto w-6 rounded-full"
+              layout
+              transition={spring}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
+
+const spring = {
+  type: "spring",
+  stiffness: 700,
+  damping: 30,
+};
 
 export default Header;
