08fa928ca7af84b0404b4003a8a534af9bb86c06
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-08-31 15:53:12 +0800

    router pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import { NavBar } from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import Home from "./Components/Routes/Home";
@@ -11,6 +12,7 @@ import SignUp from "./Components/Routes/SignUp";
 import SignForm from "./Components/Routes/SignForm";
 import EditProfile from "./Components/Routes/EditProfile";
 import Header from "./Components/Header";
+import { Post } from "./models/Post";
 export interface AppProps {}
 
 export interface AppState {}
@@ -19,28 +21,46 @@ class App extends React.Component<AppProps, AppState> {
   render() {
     return (
       <div>
-        <Header />
+        <React.Fragment>
+          <Router>
+            <Route path="/" component={Header} />
 
-        <div className="sticky top-0 z-50 bg-secondary-background">
-          <NavBar />
-        </div>
-
-        <EditProfile />
-
-        <Footer />
-        <div className="md:hidden fixed bottom-0 z-20 right-0 p-5">
-          <div className="   border-4 border-primary  bg-secondary-background p-3 text-secondary rounded-full text-xl  text-center">
-            +
-          </div>
-        </div>
-        <div className="md:block hidden fixed bottom-0 z-20 right-0 p-5">
-          <div className="border-2 border-primary  bg-secondary-background p-4 text-secondary rounded-full text-xl  text-center">
-            Add a new Post
-          </div>
-        </div>
+            <Route path="/" component={NavBar} />
+            <Route path="/" exact component={Home} />
+            <Route path="/blogs/posts" exact component={Posts} />
+            <Route path="/blogs/profile" exact component={Profile} />
+            <Route
+              path="/blogs/profile/settings"
+              exact
+              component={EditProfile}
+            />
+            <Route path="/blogs/posts/view_post" exact component={ViewPost} />
+            <Route path="/blogs/posts/add_post" exact component={AddPost} />
+            <Route path="/sign_form" exact component={SignForm} />
+            <Route path="/" component={Footer} />
+            <Route path="/" component={addButton} />
+          </Router>
+        </React.Fragment>
       </div>
     );
   }
 }
+
+const addButton = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <div className="md:hidden fixed bottom-0 z-20 right-0 p-5">
+        <div className="   border-4 border-primary  bg-secondary-background p-3 text-secondary rounded-full text-xl  text-center">
+          +
+        </div>
+      </div>
+      <div className="md:block hidden fixed bottom-0 z-20 right-0 p-5">
+        <div className="border-2 border-primary  bg-secondary-background p-4 text-secondary rounded-full text-xl  text-center">
+          Add a new Post
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default App;
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -14,60 +14,62 @@ export class NavBar extends React.Component<NavBarProps, NavBarState> {
   }
   render() {
     return (
-      <nav className="flex  items-center justify-between flex-wrap  p-6 border border-solid border-black">
-        <div className="flex items-center flex-shrink-0 text-primary mr-6">
-          <span className="font-semibold text-xl tracking-tight">
-            LOTUS BLOGS
-          </span>
-        </div>
-        <div className="block lg:hidden">
-          <button
-            onClick={() => this.setNav()}
-            className="flex items-center px-3 py-2 border rounded text-secondary border-primary hover:text-primary hover:border-secondary"
-          >
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+      <div className="sticky top-0 z-50 bg-secondary-background">
+        <nav className="flex  items-center justify-between flex-wrap  p-6 border border-solid border-black">
+          <div className="flex items-center flex-shrink-0 text-primary mr-6">
+            <span className="font-semibold text-xl tracking-tight">
+              LOTUS BLOGS
+            </span>
+          </div>
+          <div className="block lg:hidden">
+            <button
+              onClick={() => this.setNav()}
+              className="flex items-center px-3 py-2 border rounded text-secondary border-primary hover:text-primary hover:border-secondary"
             >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
+              <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
 
-        <div
-          className={` w-full transition  transform ${
-            this.state.collapsed === true
-              ? "block delay-150 duration-300 "
-              : "hidden delay-150 duration-300"
-          } flex-grow lg:flex lg:items-center lg:w-auto`}
-        >
-          <div className="text-sm lg:flex-grow text-primary">
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0  hover:text-secondary mr-4"
-            >
-              Home
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0  hover:text-secondary mr-4"
-            >
-              Posts
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0  hover:text-secondary"
-            >
-              People
-            </a>
+          <div
+            className={` w-full transition  transform ${
+              this.state.collapsed === true
+                ? "block delay-150 duration-300 "
+                : "hidden delay-150 duration-300"
+            } flex-grow lg:flex lg:items-center lg:w-auto`}
+          >
+            <div className="text-sm lg:flex-grow text-primary">
+              <a
+                href="#responsive-header"
+                className="block mt-4 lg:inline-block lg:mt-0  hover:text-secondary mr-4"
+              >
+                Home
+              </a>
+              <a
+                href="#responsive-header"
+                className="block mt-4 lg:inline-block lg:mt-0  hover:text-secondary mr-4"
+              >
+                Posts
+              </a>
+              <a
+                href="#responsive-header"
+                className="block mt-4 lg:inline-block lg:mt-0  hover:text-secondary"
+              >
+                People
+              </a>
+            </div>
+            <div>
+              <Button label="SignUp/In" />
+            </div>
           </div>
-          <div>
-            <Button label="SignUp/In" />
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     );
   }
 }
