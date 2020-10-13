299ae6894b5d67515f14de811d4de86772d846a1
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-10-13 11:17:35 +0800

    deployment v5

--- src/store/actions/modelActions.ts
+++ src/store/actions/modelActions.ts
@@ -9,7 +9,7 @@ import {
   toggling,
 } from "./dispatchTypes";
 import { ThunkDispatch } from "redux-thunk";
-//@ts-ignore
+//tslint:disable-next-line
 import { dataTypes, data } from "../types";
 import { PostAction } from "../interface";
 const auth = "Bearer ";
