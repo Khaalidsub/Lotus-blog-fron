e13ee34d475c5799397aa7156217c62002a0d99e
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-21 00:33:04 +0800

    refactoring some parts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,11 @@
 import Axios from "axios";
-// import axiosCookieJarSupport from "axios-cookiejar-support";
-// import toughCookie from "tough-cookie";
 
 export const server = "https://www.lotus-blogs-api.xyz/blog";
 export const localhost = "http://10.100.25.59:8081/blog";
-// export const localhost = "http://192.168.0.15:8083/blog";
 export const cookie = "app=";
 const axios = Axios.create({
   baseURL: server,
   withCredentials: true,
-  // headers: {
-  //   Authorization: cookie,
-  // },
-
-  // xsrfCookieName: "app",
 });
-// axiosCookieJarSupport(axios);
-// export const cookieJar = new toughCookie.CookieJar();
-// axios.defaults.jar = cookieJar;
 
 export default axios;
