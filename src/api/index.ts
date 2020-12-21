87e08b7c9cd19715d33290cced957e1d6324d1a8
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-21 13:00:12 +0800

    initial start for deploying to the ocean server

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import Axios from "axios";
 
-export const server = "https://www.lotus-blogs-api.xyz/blog";
+export const server =
+  `${process.env.SERVER}/blog` || "https://www.lotus-blogs-api.xyz/blog";
 export const localhost = "http://10.100.25.59:8081/blog";
 export const cookie = "app=";
 const axios = Axios.create({
